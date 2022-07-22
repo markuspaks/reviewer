afe67df7b4666027839156fcdc31b897e53e1436
Markus Paks <markus@trulla.ee>
2022-07-22 12:05:58 +0000

    View changes

--- resources/views/reviews/index.blade.php
+++ resources/views/reviews/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-    <div>
+    <div class="counters">
         @foreach($users as $user)
             <span class="counter">
                 <img class="avatar" src="{{ $user['links']['avatar']['href'] }}"
@@ -17,10 +17,14 @@
             @foreach($pullRequests as $pullRequest)
                 <?php $pullRequestData = $pullRequest->getData(); ?>
                 <tr>
-                    <td><img class="avatar" src="{{ $pullRequestData['author']['links']['avatar']['href'] }}"
+                    <td>
+                        <img class="avatar" src="{{ $pullRequestData['author']['links']['avatar']['href'] }}"
                              alt="{{ $pullRequestData['author']['nickname'] }}"
-                             title="{{ $pullRequestData['author']['nickname'] }}"></td>
-                    <td><a href="{{ $pullRequestData['links']['html']['href'] }}">{{ $pullRequestData['title'] }}</a>
+                             title="{{ $pullRequestData['author']['nickname'] }}">
+                    </td>
+                    <td>{{ $pullRequestData['source']['repository']['full_name'] }}</td>
+                    <td>
+                        <a href="{{ $pullRequestData['links']['html']['href'] }}">{{ $pullRequestData['title'] }}</a>
                     </td>
                     <td>
                         @foreach($pullRequestData['participants'] as $participant)
@@ -30,7 +34,9 @@
                                  title="{{ $participant['user']['nickname'] }}">
                         @endforeach
                     </td>
-                    <td>{{ (new \Carbon\Carbon($pullRequestData['updated_on']))->diffForHumans() }}</td>
+                    <td>
+                        {{ (new \Carbon\Carbon($pullRequestData['updated_on']))->diffForHumans() }}
+                    </td>
                     <td>
                         @foreach($pullRequest->getAssignees() as $assignee)
                             <img class="avatar" src="{{ $assignee['links']['avatar']['href'] }}"
